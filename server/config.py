351793101dbc9ed920bdcf43fc8908f8dffad33c
kousthubh <kyadavalli04@gmail.com>
2025-08-21 05:12:02 +0530

    updated files

--- server/config.py
+++ server/config.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 load_dotenv()
 
 class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
